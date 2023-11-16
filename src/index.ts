9e7e4ef673ef587ea127acb8f73c3bad9444ae2d
konark <konark@pop-os.localdomain>
2023-11-16 14:43:57 +0530

    added slots api

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ require('dotenv').config()
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-
 import {Request, Response, Express} from 'express';
+
+import { getSlots } from './controllers/slots.controller';
 
 
 const app: Express = express();
@@ -12,6 +13,9 @@ const port: number = 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
+
+
+app.get('/api/v1/experience/:id/slots', getSlots)
 
 app.get('/status',async (req:Request, res, Response) => {
     res.send("Server is online.")
